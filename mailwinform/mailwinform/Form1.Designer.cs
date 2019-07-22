ae528dd470c7a23d2a7b61d0a1c2fa08fc9829f0
fanzhenlol@163.com <fanzhenlol@163.com>
2019-07-22 15:38:41 +0800

    添加代码

---
+++ mailwinform/mailwinform/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace mailwinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSender = new System.Windows.Forms.TextBox();
+            this.txtReceiver = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.rtbContext = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(401, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "发件人地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "收件人地址";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "邮箱标题";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "邮件正文";
+            // 
+            // txtSender
+            // 
+            this.txtSender.Location = new System.Drawing.Point(135, 23);
+            this.txtSender.Name = "txtSender";
+            this.txtSender.Size = new System.Drawing.Size(180, 21);
+            this.txtSender.TabIndex = 6;
+            // 
+            // txtReceiver
+            // 
+            this.txtReceiver.Location = new System.Drawing.Point(135, 57);
+            this.txtReceiver.Name = "txtReceiver";
+            this.txtReceiver.Size = new System.Drawing.Size(180, 21);
+            this.txtReceiver.TabIndex = 7;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(135, 112);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(180, 21);
+            this.txtTitle.TabIndex = 9;
+            // 
+            // rtbContext
+            // 
+            this.rtbContext.Location = new System.Drawing.Point(135, 166);
+            this.rtbContext.Name = "rtbContext";
+            this.rtbContext.Size = new System.Drawing.Size(180, 96);
+            this.rtbContext.TabIndex = 10;
+            this.rtbContext.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 360);
+            this.Controls.Add(this.rtbContext);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtReceiver);
+            this.Controls.Add(this.txtSender);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSender;
+        private System.Windows.Forms.TextBox txtReceiver;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.RichTextBox rtbContext;
+    }
+}
+
---
+++ mailwinform/mailwinform/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mailwinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string smtpService = "smtp.qq.com";
+        public string sendEmail = "fanzhenmail@foxmail.com";//发送者
+        public string receiveEmail = "fanzhenlol@163.com";//接收者
+        public string sendpwd = "sxjyqgrauroqbbhc";//第三方登录码
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //确定smtp服务器地址 实例化一个Smtp客户端
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = smtpService;
+ 
+            //smtpClient.Port = "";//qq邮箱可以不用端口
+            //构建发件地址和收件地址
+            MailAddress sendAddress = new MailAddress(sendEmail, "SP的我");
+ 
+            MailAddress receiverAddress = new MailAddress(receiveEmail);//收件人
+ 
+            //构造一个Email的Message对象 内容信息
+            MailMessage message = new MailMessage(sendAddress, receiverAddress);
+            message.Subject = "邮件主题" + DateTime.Now;
+            message.SubjectEncoding = Encoding.UTF8;
+            message.Body = rtbContext.Text;
+            message.BodyEncoding = Encoding.UTF8;
+            //设置邮件的信息 如登陆密码 账号
+            //邮件发送方式  通过网络发送到smtp服务器
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //如果服务器支持安全连接，则将安全连接设为true
+            smtpClient.EnableSsl = true;
+            try
+            {
+                smtpClient.UseDefaultCredentials = false;
+                //发件用户登陆信息
+                NetworkCredential senderCredential = new NetworkCredential(sendEmail, sendpwd);
+                smtpClient.Credentials = senderCredential;
+                //发送邮件
+                smtpClient.Send(message);
+                MessageBox.Show("发送成功!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+        //    SmtpClient client = new SmtpClient();//生成SmtpClient实例，用它发送电子邮件
+        //client.Host = "smtp.qq.com";//指定SMTP服务器主机
+        //client.Port = 465;//指定要使用的端口
+        //client.Credentials = new System.Net.NetworkCredential("fanzhenmail@foxmail.com", "Fanzhen1996...");//验证用户名和密码
+        //MailMessage message = new MailMessage();
+        //MailAddress from = new MailAddress(txtSender.Text);//获取输入的发件人邮箱地址
+        //if (txtReceiver.Text != "")
+        //{
+        //    if (Regex.IsMatch(txtReceiver.Text, "\\w+@\\w+(\\.\\w+)+"))  //用正则表达式验证邮箱
+        //    {
+        //        MailAddress to = new MailAddress(txtReceiver.Text);//获取输入的收件人邮箱地址
+        //        message.To.Add(to);//设置邮件接收人，MailMessage类的To属性可以Add多个接收人
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("收件人邮箱地址不正确");
+        //        return;
+        //    }
+        //}
+        //else
+        //{
+        //    MessageBox.Show("收件人不能为空");
+        //    return;
+        //}
+
+
+      
+        //message.Subject = txtTitle.Text;//获取输入的邮件标题
+        //message.Body = rtbContext.Text;//获取输入的邮件正文
+        //message.From = from;//设置邮件发件人          
+        //client.Send(message);
+        //MessageBox.Show("发送成功");
+
+        }
+    }
+}
